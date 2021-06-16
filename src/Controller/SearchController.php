8ab96e0cf3ee299ad5192181e81ec2604cda9101
Paul VEBER <paulveber56@gmail.com>
2021-06-16 15:03:09 +0200

    search bar finaly working

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,47 +5,43 @@ namespace App\Controller;
 use App\Entity\Game;
 use App\Form\SearchType;
 use App\Repository\GameRepository;
+use Doctrine\ORM\Query;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends AbstractController
 {
-    public function search(Request $request, GameRepository $gameRepository)
+    public function searchBar()
     {
-        $result = [];
         $searchResult = new Game();
-        $form = $this->createForm(SearchType::class, $searchResult, ['csrf_protection' => false]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            dd('test');
-            $result = $gameRepository->search($searchResult->getName(), $searchResult->getCategory(), $searchResult->getTags());
-
-            return $this->redirectToRoute('search/form.html.twig', [
-                'form' => $form->createView(),
-                'data' => $searchResult,
-                'result' => $result
-            ]);
-        }
-
+        $form = $this->createForm(SearchType::class, $searchResult, [
+            'csrf_protection' => false,
+            'action' => $this->generateUrl('search')
+        ]);
         return $this->render('form/searchForm.html.twig', [
             'form' => $form->createView(),
-            'data' => $searchResult,
-            'result' => $result
         ]);
     }
-
     /**
-     * @Route("/search", name="search")
+     * @Route("/search", name="search" )
      *
      * @param Request $request
-     * @return void
      */
-    public function searchDisplay(Request $request)
+    public function search(Request $request, GameRepository $gameRepository)
     {
-        return $this->render('search/form.html.twig',[
-            'result' => $request,
+        $formData = $request->query->get('search');
+        $tags = $formData['tags'] ?? null;
+        
+        $category = !$formData['category'] ? null :  intval($formData['category']);
+        //dd($tags, $category, $formData['name']);
+        $searchResult = $gameRepository->search($formData['name'], $category, $tags);
+        //$searchResult = $gameRepository->search('','2',null);
+        return $this->render('search/form.html.twig', [
+            //'form' => $form->createView(),
+            //'data' => $searchResult,
+            'formData' => $formData,
+            'result' => $searchResult
         ]);
     }
 }
